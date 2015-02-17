2269f3b475db295fafb986fab7f9a79e1fb5fd43
Nicholas Muesch <mueschn@gmail.com>
2015-02-17 18:18:31 -0500

    Tests isValid for the Exclusive Unary Constraint class.

--- src/CSP/ExclusiveUnary.java
+++ src/CSP/ExclusiveUnary.java
@@ -21,8 +21,44 @@ public class ExclusiveUnary implements IConstraint {
 	
 	@Override
 	public boolean isValid(ArrayList<Assignment> assignments) {
-		// TODO Auto-generated method stub
-		return false;
+			for(Assignment a : assignments) {
+				if(a.getItem().equals(item)) {
+					return !(bags.contains(a.getBag()));
+				}
+			}
+			return true;
+		}
+	
+	
+	public static void main(String[] args) {
+		Item i1  = new Item('a', 10);
+		Item i2 = new Item('b', 15);
+		Bag b1 = new Bag('A', 50);
+		Bag b2 = new Bag('B', 20);
+		
+		ArrayList<Bag> bags = new ArrayList<Bag>();
+		bags.add(b1);
+		
+		ExclusiveUnary EU = new ExclusiveUnary(i1, bags);
+		
+		Assignment a1 = new Assignment(b1, i1);
+		Assignment a2 = new Assignment(b2, i2);
+		
+		ArrayList<Assignment> ass = new ArrayList<Assignment>();
+		
+		ass.add(a1);
+		ass.add(a2);
+		
+		ArrayList<Assignment> ass2 = new ArrayList<Assignment>();
+		ass2.add(a2);
+		
+		//Should be false
+		System.out.println(EU.isValid(ass));
+		
+		//Should be true
+		System.out.println(EU.isValid(ass2));
 	}
 
 }
+
+
